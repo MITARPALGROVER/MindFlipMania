1e82aba44b6faf9e5354bbb59c7d8949e791e3b2
MITARPAL GROVER <mitar10pal@gmail.com>
2025-04-09 13:32:28 +0530

    temporary-check for api request

--- login/login.js
+++ login/login.js
@@ -38,7 +38,7 @@ async function validateLogin() {
   }
 
   try {
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://mindflipmania.vercel.app/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
