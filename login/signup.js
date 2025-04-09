0203484a4b88347cf017e5729327a4e6dae6d5e7
MITARPAL GROVER <mitar10pal@gmail.com>
2025-04-09 13:54:46 +0530

    reversed the changes

--- login/signup.js
+++ login/signup.js
@@ -46,7 +46,7 @@ async function validateForm() {
     }
   
     try {
-      const response = await fetch("https://mindflipmania.vercel.app/api/auth/register", {
+      const response = await fetch("http://localhost:5000/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
